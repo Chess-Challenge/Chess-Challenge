8fb93667193b8586629a392df02dfc9fa2b609ef
Laurin Zehnder <laurin.zehnder@ticos.ch>
2023-07-24 17:04:54 +0200

    NegamaxV2

--- Chess-Challenge/src/Evil Bot/EvilBot.cs
+++ Chess-Challenge/src/Evil Bot/EvilBot.cs
@@ -1,54 +1,183 @@
 ﻿using ChessChallenge.API;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace ChessChallenge.Example
+namespace ChessChallenge.Example;
+
+public class EvilBot : IChessBot
 {
-    // A simple bot that can spot mate in one, and always captures the most valuable piece it can.
-    // Plays randomly otherwise.
-    public class EvilBot : IChessBot
+    int[] pieceValues = { 0, 1, 3, 3, 5, 9, 200 };
+    static Random random = new();
+
+    Dictionary<ulong, float> transpositionTable;
+    int level;
+
+    public Move Think(Board board, Timer timer)
     {
-        // Piece values: null, pawn, knight, bishop, rook, queen, king
-        int[] pieceValues = { 0, 100, 300, 300, 500, 900, 10000 };
+        level = 4;
 
-        public Move Think(Board board, Timer timer)
+        transpositionTable = new Dictionary<ulong, float>();
+        var (expectedScore, move) = MiniMax(board, level, float.NegativeInfinity, float.PositiveInfinity, true, board.IsWhiteToMove, timer);
+
+        if (move.IsNull)
         {
-            Move[] allMoves = board.GetLegalMoves();
+            Console.WriteLine("NullMove");
+        }
+        board.MakeMove(move);
+        float value = -board.GetAllPieceLists().Sum(pl => pl.Sum(p => (p.IsWhite == board.IsWhiteToMove) ? pieceValues[(int)p.PieceType] : -pieceValues[(int)p.PieceType]));
+        board.UndoMove(move);
 
-            // Pick a random move to play if nothing better is found
-            Random rng = new();
-            Move moveToPlay = allMoves[rng.Next(allMoves.Length)];
-            int highestValueCapture = 0;
+        Console.WriteLine(
+            $"{(board.IsWhiteToMove ? "White" : "Black")} {move} " +
+            $"-> Value: {(float.IsInfinity(value) ? $"{(float.IsPositiveInfinity(value) ? "+" : "-")}Infinity" : $"{value:.00}")} " +
+            $"(Expected: {(float.IsInfinity(expectedScore) ? $"{(float.IsPositiveInfinity(expectedScore) ? "+" : "-")}Infinity" : $"{expectedScore:.00}")})");
 
-            foreach (Move move in allMoves)
+        return move;
+    }
+
+    (float, Move) MiniMax(Board board, int depth, float alpha, float beta, bool maximizer, bool isWhite, Timer timer)
+    {
+        if (depth == 0 /*|| timer.MillisecondsElapsedThisTurn > 1_500*/)
+        {
+            if (board.IsInCheckmate())
             {
-                // Always play checkmate in one
-                if (MoveIsCheckmate(board, move))
+                return (float.PositiveInfinity, Move.NullMove);
+            }
+
+            float value = board.GetAllPieceLists().Sum(pl => pl.Sum(p => (p.IsWhite == isWhite/*board.IsWhiteToMove*/) ? pieceValues[(int)p.PieceType] : -pieceValues[(int)p.PieceType]));
+            value += (0.1f * board.GetLegalMoves().Length);
+
+
+            /*if (value == 0)
+            {
+                if (random.NextDouble() > 0.5)
                 {
-                    moveToPlay = move;
-                    break;
+                    value = +0.5f;
+                }
+                else
+                {
+                    value = -0.5f;
+                }
+            }*/
+
+            return (value, Move.NullMove);
+        }
+
+        var moves = board.GetLegalMoves();
+        var chosen = Move.NullMove;
+
+        if (maximizer)
+        {
+            var maxEval = float.NegativeInfinity;
+
+            foreach (var move in moves)
+            {
+                board.MakeMove(move);
+
+                if (board.IsInCheckmate())
+                {
+                    board.UndoMove(move);
+                    transpositionTable.TryAdd(board.ZobristKey, float.PositiveInfinity);
+
+                    return (float.PositiveInfinity, move);
                 }
 
-                // Find highest value capture
-                Piece capturedPiece = board.GetPiece(move.TargetSquare);
-                int capturedPieceValue = pieceValues[(int)capturedPiece.PieceType];
-
-                if (capturedPieceValue > highestValueCapture)
+                if (board.IsDraw())
                 {
-                    moveToPlay = move;
-                    highestValueCapture = capturedPieceValue;
+                    board.UndoMove(move);
+                    continue;
+                }
+
+                if (!transpositionTable.TryGetValue(board.ZobristKey, out float eval))
+                {
+                    eval = MiniMax(board, depth - 1, alpha, beta, false, isWhite, timer).Item1;
+
+                    transpositionTable.TryAdd(board.ZobristKey, eval);
+                }
+
+
+                if (eval > maxEval)
+                {
+                    maxEval = eval;
+                    chosen = move;
+                }
+                alpha = Math.Max(alpha, eval);
+
+                board.UndoMove(move);
+
+                if (beta <= alpha)
+                {
+                    break;
                 }
             }
 
-            return moveToPlay;
-        }
+            if (chosen == Move.NullMove)
+            {
+                if (moves.Length == 0)
+                {
+                    return (maxEval, Move.NullMove);
+                }
 
-        // Test if this move gives checkmate
-        bool MoveIsCheckmate(Board board, Move move)
+                chosen = moves[random.Next(moves.Length)];
+            }
+            return (maxEval, chosen);
+        }
+        else
         {
-            board.MakeMove(move);
-            bool isMate = board.IsInCheckmate();
-            board.UndoMove(move);
-            return isMate;
+            var minEval = float.PositiveInfinity;
+
+            foreach (var move in moves)
+            {
+                board.MakeMove(move);
+
+                if (board.IsInCheckmate())
+                {
+                    board.UndoMove(move);
+                    transpositionTable.TryAdd(board.ZobristKey, float.NegativeInfinity);
+
+                    return (float.NegativeInfinity, move);
+                }
+
+                if (board.IsDraw())
+                {
+                    board.UndoMove(move);
+                    continue;
+                }
+
+
+                if (!transpositionTable.TryGetValue(board.ZobristKey, out float eval))
+                {
+                    eval = MiniMax(board, depth - 1, alpha, beta, true, isWhite, timer).Item1;
+
+                    transpositionTable.TryAdd(board.ZobristKey, eval);
+                }
+
+                if (eval < minEval)
+                {
+                    minEval = eval;
+                    chosen = move;
+                }
+                beta = Math.Min(beta, eval);
+
+                board.UndoMove(move);
+
+                if (beta <= alpha)
+                {
+                    break;
+                }
+            }
+
+            if (chosen == Move.NullMove)
+            {
+                if (moves.Length == 0)
+                {
+                    return (minEval, Move.NullMove);
+                }
+
+                chosen = moves[random.Next(moves.Length)];
+            }
+            return (minEval, chosen);
         }
     }
 }
--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -13,33 +13,37 @@ public class MyBot : IChessBot
 
     public Move Think(Board board, Timer timer)
     {
-        level = 6;
+        level = 4;
 
-        transpositionTable = new Dictionary<ulong, float>();
-        var (expectedScore, move) = MiniMax(board, level, float.NegativeInfinity, float.PositiveInfinity, true, board.IsWhiteToMove, timer);
+        transpositionTable = new();
+        var (expectedScore, move) = NegaMax(board, level, float.NegativeInfinity, float.PositiveInfinity, timer);
 
+        if (move.IsNull)
+        {
+            Console.WriteLine("NullMove");
+        }
         board.MakeMove(move);
-        var score = MiniMax(board, 0, float.NaN, float.NaN, true, !board.IsWhiteToMove, timer).Item1;
+        float value = -board.GetAllPieceLists().Sum(pl => pl.Sum(p => (p.IsWhite == board.IsWhiteToMove) ? pieceValues[(int)p.PieceType] : -pieceValues[(int)p.PieceType]));
         board.UndoMove(move);
 
         Console.WriteLine(
-            $"{(board.IsWhiteToMove ? "White" : "Black")} {move} " +
-            $"-> Score: {(float.IsInfinity(score) ? $"{(float.IsPositiveInfinity(score) ? "+" : "-")}Infinity" : $"{score:.00}")} " +
-            $"(Expected: {(float.IsInfinity(expectedScore) ? $"{(float.IsPositiveInfinity(expectedScore) ? "+" : "-")}Infinity" : $"{expectedScore:.00}")})");
+                $"{(board.IsWhiteToMove ? "White" : "Black")} {move} " +
+                $"-> Value: {(float.IsInfinity(value) ? $"{(float.IsPositiveInfinity(value) ? "+" : "-")}Infinity" : $"{value:.00}")} " +
+                $"(Expected: {(float.IsInfinity(expectedScore) ? $"{(float.IsPositiveInfinity(expectedScore) ? "+" : "-")}Infinity" : $"{expectedScore:.00}")})");
 
         return move;
     }
 
-    (float, Move) MiniMax(Board board, int depth, float alpha, float beta, bool maximizer, bool isWhite, Timer timer)
+    (float, Move) NegaMax(Board board, int depth, float alpha, float beta, Timer timer)
     {
+        if (board.IsInCheckmate())
+        {
+            return (99999, Move.NullMove);
+        }
+
         if (depth == 0 /*|| timer.MillisecondsElapsedThisTurn > 1_500*/)
         {
-            if (board.IsInCheckmate())
-            {
-                return (float.PositiveInfinity, Move.NullMove);
-            }
-
-            float value = board.GetAllPieceLists().Sum(pl => pl.Sum(p => (p.IsWhite == isWhite/*board.IsWhiteToMove*/) ? pieceValues[(int)p.PieceType] : -pieceValues[(int)p.PieceType]));
+            float value = board.GetAllPieceLists().Sum(pl => pl.Sum(p => (p.IsWhite == board.IsWhiteToMove) ? pieceValues[(int)p.PieceType] : -pieceValues[(int)p.PieceType]));
             value += (0.1f * board.GetLegalMoves().Length);
 
 
@@ -55,123 +59,46 @@ public class MyBot : IChessBot
                 }
             }
 
-            return /*(level % 2 == 0 ? value : -value, Move.NullMove)*/(value, Move.NullMove);
+            return (value, Move.NullMove);
         }
 
         var moves = board.GetLegalMoves();
         var chosen = Move.NullMove;
 
-        if (maximizer)
+        var maxEval = float.NegativeInfinity;
+
+        foreach (var move in moves)
         {
-            var maxEval = float.NegativeInfinity;
-
-            foreach (var move in moves)
+            board.MakeMove(move);
+            
+            if (board.IsDraw())
             {
-                board.MakeMove(move);
-
-                if (board.IsInCheckmate())
-                {
-                    board.UndoMove(move);
-                    transpositionTable.TryAdd(board.ZobristKey, float.PositiveInfinity);
-
-                    return (float.PositiveInfinity, move);
-                }
-
-                if (board.IsDraw())
-                {
-                    board.UndoMove(move);
-                    continue;
-                }
-
-                if (!transpositionTable.TryGetValue(board.ZobristKey, out float eval))
-                {
-                    eval = MiniMax(board, depth - 1, alpha, beta, false, isWhite, timer).Item1;
-
-                    transpositionTable.TryAdd(board.ZobristKey, eval);
-                }
-
-
-                if (eval > maxEval)
-                {
-                    maxEval = eval;
-                    chosen = move;
-                }
-                alpha = Math.Max(alpha, eval);
-
                 board.UndoMove(move);
-
-                if (beta <= alpha)
-                {
-                    break;
-                }
+                continue;
             }
 
-            if (chosen == Move.NullMove)
+            if (!transpositionTable.TryGetValue(board.ZobristKey, out float eval))
             {
-                if (moves.Length == 0)
-                {
-                    return (maxEval, Move.NullMove);
-                }
+                eval = -NegaMax(board, depth - 1, -beta, -alpha, timer).Item1;
 
-                chosen = moves[random.Next(moves.Length)];
+                transpositionTable.Add(board.ZobristKey, eval);
             }
-            return (maxEval, chosen);
+            
+            if (eval > maxEval)
+            {
+                maxEval = eval;
+                chosen = move;
+            }
+            alpha = Math.Max(alpha, eval);
+
+            board.UndoMove(move);
+
+            if (beta <= alpha)
+            {
+                break;
+            }
         }
-        else
-        {
-            var minEval = float.PositiveInfinity;
 
-            foreach (var move in moves)
-            {
-                board.MakeMove(move);
-
-                if (board.IsInCheckmate())
-                {
-                    board.UndoMove(move);
-                    transpositionTable.TryAdd(board.ZobristKey, float.NegativeInfinity);
-
-                    return (float.NegativeInfinity, move);
-                }
-
-                if (board.IsDraw())
-                {
-                    board.UndoMove(move);
-                    continue;
-                }
-
-
-                if (!transpositionTable.TryGetValue(board.ZobristKey, out float eval))
-                {
-                    eval = MiniMax(board, depth - 1, alpha, beta, true, isWhite, timer).Item1;
-
-                    transpositionTable.TryAdd(board.ZobristKey, eval);
-                }
-
-                if (eval < minEval)
-                {
-                    minEval = eval;
-                    chosen = move;
-                }
-                beta = Math.Min(beta, eval);
-
-                board.UndoMove(move);
-
-                if (beta <= alpha)
-                {
-                    break;
-                }
-            }
-
-            if (chosen == Move.NullMove)
-            {
-                if (moves.Length == 0)
-                {
-                    return (minEval, Move.NullMove);
-                }
-
-                chosen = moves[random.Next(moves.Length)];
-            }
-            return (minEval, chosen);
-        }
+        return (maxEval, chosen);
     }
 }
